c27e9b18084e6ebcf96c43bedd57e243d5de4e61
adriana <adriana.mihalescu@gmail.com>
2021-11-04 19:27:30 +0000

    added background style, helper functions and button logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from "./components/Layout/Layout";
 import QuoteContainer from "./components/QuoteContainer/QuoteContainer";
 import SocialButton from "./components/SocialButton/SocialButton";
 import quoteData from "./QuotesData";
-import { gsap } from "gsap";
 
 const randomNumber = (min, max) => {
 	min = Math.ceil(min);
@@ -16,14 +15,36 @@ function App() {
 	const queryParams = new URLSearchParams(window.location.search);
 	const currentID = queryParams.get("id");
 
+	const bgColors = [
+		{
+			backgroundColor: "#fee2f8",
+			backgroundImage: "linearGradient(315deg, #fee2f8 0%, #dcf8ef 74%)",
+		},
+		{
+			backgroundColor: "#2884b8",
+			backgroundImage: "linear-gradient(315deg, #2884b8 0%, #d1bad2 74%)",
+		},
+		{
+			backgroundColor: " #70b2d9;",
+			backgroundImage: "linear-gradient(315deg, #70b2d9 0%, #39e5b6 74%)",
+		},
+		{
+			backgroundColor: "#fffffc",
+			backgroundImage: "linear-gradient(315deg, #fffffc 0%, #beb7a4 74%)",
+		},
+	];
+
 	const [activeQuote, setActiveQuote] = useState();
+	const [activeBG, setActiveBG] = useState(bgColors[0]);
 
 	const buttonHandler = () => {
-		const random = randomNumber(0, 21);
+		const randomQ = randomNumber(0, 21);
+		const randomBG = randomNumber(0, 3);
 
-		setActiveQuote(quoteData[random]);
+		setActiveQuote(quoteData[randomQ]);
+		setActiveBG(bgColors[randomBG]);
 
-		var newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?id=${random}`;
+		var newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?id=${randomQ}`;
 		window.history.pushState({ path: newurl }, "", newurl);
 	};
 
@@ -36,7 +57,7 @@ function App() {
 	}, [currentID]);
 
 	return (
-		<Layout>
+		<Layout style={{ ...activeBG }}>
 			<header>
 				<h1>Random quote machine</h1>
 			</header>
--- src/components/QuoteContainer/QuoteContainer.js
+++ src/components/QuoteContainer/QuoteContainer.js
@@ -1,15 +1,61 @@
 import "./QuoteContainer.css";
 import Button from "../Button/Button";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { split } from "../../helpers";
 
 const QuoteContainer = ({ quote, author, buttonHandler }) => {
+	const quoteTextRef = useRef(null);
+	const authorTextRef = useRef(null);
+
+	useEffect(() => {
+		if (!quoteTextRef.current || !authorTextRef.current) return;
+		const quoteChars = split({ element: quoteTextRef.current, append: true });
+		const authorChars = split({ element: authorTextRef.current, append: true });
+
+		gsap.set(quoteTextRef.current, { perspective: 400 });
+
+		gsap.from(
+			quoteChars,
+			{
+				duration: 0.1,
+				opacity: 0,
+				scale: 1,
+				delay: 0.4,
+				y: -40,
+				rotationX: -90,
+				transformOrigin: "0% 50% -50",
+				ease: "inOut",
+				stagger: 0.045,
+			},
+			"+=0"
+		);
+
+		// gsap.from(
+		// 	authorChars,
+		// 	{
+		// 		duration: 0.1,
+		// 		opacity: 0,
+		// 		scale: 1,
+		// 		delay: 0.4,
+		// 		y: -40,
+		// 		rotationX: -90,
+		// 		transformOrigin: "0% 50% -50",
+		// 		ease: "inOut",
+		// 		stagger: 0.045,
+		// 	},
+		// 	"+=0"
+		// );
+	}, [quote]);
+
 	return (
 		<div className="container">
 			<div id="quote-box" className="quote-box">
 				<div id="text" className="quote-text">
-					{quote}
+					<p ref={quoteTextRef}>{quote}</p>
 				</div>
 				<div id="author" className="quote-author">
-					{author}
+					<span ref={authorTextRef}>{`Author:${author}`}</span>
 				</div>
 			</div>
 			<div className="toolbar">
